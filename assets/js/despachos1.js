b6222c774a58121860d04674c8e3183bf12ae650
Jhon-laboratory <jhonlarico11@gmail.com>
2026-03-11 23:19:50 -0400

    historial9

--- assets/js/despachos1.js
+++ assets/js/despachos1.js
@@ -467,7 +467,7 @@ async function enviarDatosInterfaz() {
         const controller = new AbortController();
         const timeoutId = setTimeout(() => controller.abort(), 60000);
         
-        const response = await fetch('../Controller/enviarsftp.php', {
+        const response = await fetch('../controller/enviarsftp.php', {
             method: 'POST',
             headers: { 
                 'Content-Type': 'application/json',
